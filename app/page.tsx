30dc1a32aa2ef8b56da0347467ac395006280c5e
afirdousi <anas.firdousi@gmail.com>
2024-07-11 15:01:30 -0700

    Abiding by react/no-unescaped-entities ES Lint rule

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
-import { Hero } from "./components/Hero"
+import Link from "next/link";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
@@ -8,12 +8,12 @@ export default function Home() {
         <div className="hero-wrapper">
           <Hero title="Hey, I'm Anas" index>
             <p className="hero-description">
-              I'm a software engineering turned engineering leader. Wanabe enterpreneur. I'm passionate about building great products, teams, and communities.
-              I write about distributed systems, machine learning , leadership, and personal growth.
+              I&apos;m a software engineering turned engineering leader. Wanabe entrepreneur. I&apos;m passionate about building great products, teams, and communities.
+              I write about distributed systems, machine learning, leadership, and personal growth.
             </p>
             <p className="hero-description">
               On this site, you can check out all the{' '}
-              <Link href="/blog">technical articles</Link> I've written, read some
+              <Link href="/blog">technical articles</Link> I&apos;ve written, read some
               of my <Link href="/notes">learnings</Link>, or learn more{' '}
               <Link href="/me">about me</Link>.
             </p>
@@ -28,8 +28,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
